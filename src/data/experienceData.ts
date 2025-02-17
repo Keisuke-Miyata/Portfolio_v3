d7e8945dac99495f71cbf3fb22dcf723267f841a
Keisuke Miyata <83644509+Keisuke-Miyata@users.noreply.github.com>
2025-02-17 01:54:45 +0100

    modify experience data

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -1,32 +1,32 @@
 // data/experienceData.ts
 export const experienceData = [
-    {
-        title: "Software Engineer",
-        company: "TechCorp",
-        description:
-            "Developed and maintained scalable web applications. Developed and maintained scalable web applications. Developed and maintained scalable web applications.",
-        startDate: "Jul 2021",
-        endDate: "Present",
-        technologies: [],
-        companyLink: "https://stackoverflow.com/",
-    },
-    {
-        title: "Frontend Developer",
-        company: "WebSolutions",
-        description:
-            "Worked on responsive UI/UX improvements and frontend optimization.",
-        startDate: "Jan 2020",
-        endDate: "Jun 2021",
-        technologies: ["React", "TypeScript", "Sass"],
-        companyLink: "https://example.com/",
-    },
-    {
-        title: "Junior Developer",
-        company: "CodeStudio",
-        description:"Collaborated on multiple front-end projects using React.",
-        startDate:"Jul 2022",
-        endDate:"Dec 2022",
-        technologies:["JavaScript", "TypeScript", "SCSS", "REACT", "HTML"],
-        companyLink:"https://stackoverflow.com/",
-    }
+    // {
+    //     title: "Software Engineer",
+    //     company: "TechCorp",
+    //     description:
+    //         "Developed and maintained scalable web applications. Developed and maintained scalable web applications. Developed and maintained scalable web applications.",
+    //     startDate: "Jul 2021",
+    //     endDate: "Present",
+    //     technologies: [],
+    //     companyLink: "https://stackoverflow.com/",
+    // },
+    // {
+    //     title: "Frontend Developer",
+    //     company: "WebSolutions",
+    //     description:
+    //         "Worked on responsive UI/UX improvements and frontend optimization.",
+    //     startDate: "Jan 2020",
+    //     endDate: "Jun 2021",
+    //     technologies: ["React", "TypeScript", "Sass"],
+    //     companyLink: "https://example.com/",
+    // },
+    // {
+    //     title: "Junior Developer",
+    //     company: "CodeStudio",
+    //     description:"Collaborated on multiple front-end projects using React.",
+    //     startDate:"Jul 2022",
+    //     endDate:"Dec 2022",
+    //     technologies:["JavaScript", "TypeScript", "SCSS", "REACT", "HTML"],
+    //     companyLink:"https://stackoverflow.com/",
+    // }
 ];
